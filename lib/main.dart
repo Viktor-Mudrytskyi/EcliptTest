44e7fe8f451c60ae20f34b61bc056b165e202211
Mudrytskyi <mudritskiy.vk@gmail.com>
2025-09-18 10:31:41 +0300

    ios config

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:test_task_eclipt/service/di_service.dart';
 import 'package:test_task_eclipt/view/app.dart';
 
-void main() {
+void main() async {
   _setup();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MainApp());
 }
 
---
+++ lib/theme/app_images.dart
@@ -0,0 +1,7 @@
+part of 'resources.dart';
+
+class AppImages {
+  AppImages._();
+
+  static const String splashLogo = 'asset/image/splash_logo.png';
+}
